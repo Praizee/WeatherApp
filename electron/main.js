dd308ec51739fd440817aaf9c6ff96497e1efe24
Stephen <praiseadeniji2017@gmail.com>
2026-05-05 23:46:49 +0100

    fix: use app.isPackaged for dev detection instead of electron-is-dev

--- electron/main.js
+++ electron/main.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow, ipcMain, Menu } = require('electron');
 const path = require('path');
-const isDev = require('electron-is-dev');
+const isDev = !app.isPackaged;
 const { buildMenu } = require('./menu');
 
 let mainWindow;
